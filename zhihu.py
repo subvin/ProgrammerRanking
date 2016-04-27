48ecd423f6ae6569d9defcc927fc0ed618bec6ae
subvin <root@subvin.com>
2016-04-27 22:48:59 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -14,7 +14,7 @@ global startNum
 
 dataList = []
 print '\t\t\t\t This Is  Our 		Snaillove 		Team 		Zhihu 		Ranking \n'
-for i in xrange(0,len(ids)): #
+for i in xrange(0,len(ids)): #   
 	url = re.sub('id','%s'%ids[i],baseurl,re.S)
 	wp = urllib2.urlopen(url)
 	content = wp.read()
@@ -46,19 +46,58 @@ for i in xrange(0,len(ids)): #
 	aData = (ids[i],int(agreeNum),int(thanksNum),int(askNum[0]),int(askNum[1]),int(askNum[2]),int(askNum[3]),focusNum[0],focusNum[1],focus,re.findall(r'(.*? )',proFocus[1],re.S)[0],beReviewedNumber)
 	dataList.append(aData)
 
-f = open('/Users/wangyunfeng/Desktop/zhihu.txt','w')
-title2 = ('个人ID ',' 赞同数','感谢数','提问数','回答数','文章数','收藏','关注了','关注者',' 专栏数','话题数','被浏览次数')
-title = titleFormatter% title2 #('id','Agree Number','Thanks Number','Ask Number','Answer Number','Article Number','Collections','Focus Number','Be Focused','Special Column','Topics','Be Reviewed Number')
+#f = open('/Users/wangyunfeng/Desktop/zhihu.txt','w')
+title2 = ('总排名','个人ID ',' 赞同数','感谢数','提问数','回答数','文章数','收藏','关注了','关注者',' 专栏数','话题数','被浏览次数')
+#title = titleFormatter% title2 #('id','Agree Number','Thanks Number','Ask Number','Answer Number','Article Number','Collections','Focus Number','Be Focused','Special Column','Topics','Be Reviewed Number')
 
-print title
-f.writelines(title)
+#print title
+#f.writelines(title)
 
 #print titleFormatter%('id','Agree Number','Thanks Number','Ask Number','Answer Number','Article Number','Collections','Focus Number','Be Focused','Special Column','Topics','Be Reviewed Number')
 newDataList =  sorted(dataList, key=itemgetter(1,2,8,6,11),reverse = True)
+#for x in xrange(0,len(newDataList)):
+#	DataContent =  contentFormatter % (newDataList[x])
+#	f.writelines(DataContent)
+#	print contentFormatter % (newDataList[x])
+
+#f.writelines(newDataList)
+#f.close()
+
+
+
+
+f = open('/Users/wangyunfeng/Desktop/Zhihu.txt','w')
+
+ranktitle = ''
+for x in xrange(0,len(title2)):
+	ranktitle += '|%s'%title2[x]
+ranktitle += '    \n'
+print ranktitle
+f.write(ranktitle)
+tableFormatter = ''
+for x in xrange(0,len(title2)):
+	tableFormatter += '|---'
+tableFormatter += '|    \n'
+f.write(tableFormatter)
+
+
+#print titleFormatter%('id','Agree Number','Thanks Number','Ask Number','Answer Number','Article Number','Collections','Focus Number','Be Focused','Special Column','Topics','Be Reviewed Number')
+
 for x in xrange(0,len(newDataList)):
-	DataContent =  contentFormatter % (newDataList[x])
-	f.writelines(DataContent)
-	print contentFormatter % (newDataList[x])
+	rankContent = '|#%i'% int(x+1)
+	DataContent =  newDataList[x]
+	for j in xrange(0,len(DataContent)):
+		if 0 == j:
+			rankContent += '|[@%s](%s)'%(DataContent[0],re.sub('id','%s'%DataContent[0],baseurl,re.S))
+		else:
+			rankContent += '|%s'%DataContent[j]
+	rankContent += '    \n'
+	f.write(rankContent)
+print rankContent
+
+
+
+#	print contentFormatter % (newDataList[x])
 
 #f.writelines(newDataList)
 f.close()
