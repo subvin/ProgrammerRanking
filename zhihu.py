01cce0a1c86560535c788b0ff573fd51ceb75700
subvin <root@subvin.com>
2016-06-01 15:32:10 +0800

    Update zhihu.py
    修改ID。

--- zhihu.py
+++ zhihu.py
@@ -3,7 +3,7 @@
 import urllib2,re
 from operator import itemgetter, attrgetter 
 
-ids = ['ifeegoo','chiemy','misparking','wfiskz','subvin','iOnesmile','Jackwaiting','chen-yun-xuan-29','arrfu','yonghelu','li-hong-yuan-54','lan-jie-82-66','huangshuhan']
+ids = ['ifeegoo','chiemy','misparking','wfiskz','subvin','iOnesmile','Jackwaiting','arrfu','yonghelu','lan-jie-82-66','huangshuhan','chenyunxuan','Lewanny']
 
 titleFormatter =   "\t%20s  \t\t%10s \t \t%10s \t    \t\t%10s \t \t\t  %10s \t \t %10s \t \t %10s \t \t %10s \t \t%10s \t \t%10s \t \t%10s \t  \t%20s \t"
 contentFormatter = "%20s  %10s \t %10s \t %10s \t  %10s \t %10s \t %10s \t %10s \t %10s  %10s \t %10s \t %10s \t"
@@ -16,6 +16,7 @@ dataList = []
 print '\t\t\t\t This Is  Our 		Snaillove 		Team 		Zhihu 		Ranking \n'
 for i in xrange(0,len(ids)): #   
 	url = re.sub('id','%s'%ids[i],baseurl,re.S)
+	print url
 	wp = urllib2.urlopen(url)
 	content = wp.read()
 	agreeNum = re.findall(r'<span class="zm-profile-header-user-agree"><span class="zm-profile-header-icon"></span><strong>(.*?)</strong>',content,re.S)[0]
@@ -35,8 +36,8 @@ for i in xrange(0,len(ids)): #
 		focus = proFocus[0]
 	else:
 		focus = realProFocusNum[0]
-
 	aboutUrl = re.sub('id','%s'%ids[i],abouturl,re.S)
+	print 'AboutUrl = %s'%aboutUrl
 	header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'}
 	req = urllib2.Request(url = aboutUrl,headers = header)
 	#opener = urllib2.build_opener()
@@ -45,6 +46,7 @@ for i in xrange(0,len(ids)): #
 	#print collectionPage
 	aData = (ids[i],int(agreeNum),int(thanksNum),int(askNum[0]),int(askNum[1]),int(askNum[2]),int(askNum[3]),focusNum[0],focusNum[1],focus,re.findall(r'(.*? )',proFocus[1],re.S)[0],beReviewedNumber)
 	dataList.append(aData)
+	wp.close()
 
 #f = open('/Users/wangyunfeng/Desktop/zhihu.txt','w')
 title2 = ('总排名','个人ID ',' 赞同数','感谢数','提问数','回答数','文章数','收藏','关注了','关注者',' 专栏数','话题数','被浏览次数')
